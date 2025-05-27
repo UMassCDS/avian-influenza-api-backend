dfe180f38ecdbd0021cdff6b0fff5518b7d3070f
LyraDusk <lauren@meyerzone.net>
2025-05-27 12:30:18 -0400

    Beginning BF API miplementation

--- api/endpoints/flow_wrap_dev.R
+++ api/endpoints/flow_wrap_dev.R
@@ -1,10 +1,30 @@
 require(plumber)
 
 #* @param date The date to be echoed in the response
+#* @param taxa Species to include
+#* @param loc
+#* @param n
 #* @get /flow
 # Sample: http://0.0.0.0:8000/flow?date=text
-function(date = "") {
+function(date = "", taxa = "amewoo", lat, lon, n, direction = "forward") {
+  start <- as.Date(date)
+  n <- as.numeric(n)
+  
+  model <- BirdFlowModels::amewoo
+  
+  xy <- BirdFlowR::latlon_to_xy(lat, lon, model)
+  dist <- BirdFlowR::as_distr(xy, model)
+  
+  out_dist <- predict(model, distr = dist, start = start, n_steps = n)
+  
+  for (i in 1:n) {
+    plot_distr(out_dist[i], model)
+  }
+  # upload those somehow and get the URLs
+  
   list(
-    message_echo = paste(date, "is a date!")
+    date = paste(start),
+    lag = paste(n),
+    taxa = paste(taxa)
   )
 }
