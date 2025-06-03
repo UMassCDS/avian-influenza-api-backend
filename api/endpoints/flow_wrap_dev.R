a3ba0131dbf330741ff610b140dfdc92566406a9
LyraDusk <lauren@meyerzone.net>
2025-06-03 17:27:54 -0400

    Model Selection and Import (single model)

--- api/endpoints/flow_wrap_dev.R
+++ api/endpoints/flow_wrap_dev.R
@@ -2,29 +2,45 @@ require(plumber)
 
 #* @param date The date to be echoed in the response
 #* @param taxa Species to include
-#* @param loc
 #* @param n
 #* @get /flow
 # Sample: http://0.0.0.0:8000/flow?date=text
-function(date = "", taxa = "amewoo", lat, lon, n, direction = "forward") {
+flow <- function(date = "", taxa = "amewoo", lat, lon, n, direction = "forward") {
+  
+  birdflow_options(collection_url = "https://birdflow-science.s3.amazonaws.com/collection/")
+  index <- load_collection_index()
+  model <- NULL
+  
+  if (taxa %in% index$species_code) {
+    modelname <- index |>
+      dplyr::filter(species_code == taxa) |>
+      dplyr::select(model)
+    print(modelname$model)
+    model <- load_model(modelname$model)
+    }
+  
   start <- as.Date(date)
   n <- as.numeric(n)
   
-  model <- BirdFlowModels::amewoo
+  
   
   xy <- BirdFlowR::latlon_to_xy(lat, lon, model)
   dist <- BirdFlowR::as_distr(xy, model)
   
   out_dist <- predict(model, distr = dist, start = start, n_steps = n)
   
+  out <- list()
+  
   for (i in 1:n) {
     plot_distr(out_dist[i], model)
+    out[i] <- c(
+      date = paste(start),
+      lag = paste(n),
+      taxa = paste(taxa)
+    )
   }
   # upload those somehow and get the URLs
   
-  list(
-    date = paste(start),
-    lag = paste(n),
-    taxa = paste(taxa)
-  )
+  out
+
 }
