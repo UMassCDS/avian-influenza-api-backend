56bc82451c7dba866d004c4829887792d7223e29
LyraDusk <lauren@meyerzone.net>
2025-06-09 15:28:22 -0400

    Return formatting setup.

--- api/endpoints/flow_wrap_dev.R
+++ api/endpoints/flow_wrap_dev.R
@@ -28,16 +28,24 @@ flow <- function(date = "", taxa = "amewoo", lat, lon, n, direction = "forward")
   dist <- BirdFlowR::as_distr(xy, model)
   
   out_dist <- predict(model, distr = dist, start = start, n_steps = n)
+  out_rast <- rasterize_distr(out_dist, model)
   
-  out <- list()
+  out <- list(c())
   
   for (i in 1:n) {
-    plot_distr(out_dist[i], model)
-    out[i] <- c(
-      date = paste(start),
-      lag = paste(n),
-      taxa = paste(taxa)
-    )
+    i_print <- i
+    if (direction == "inflow") {
+      i_print <- -i
+    }
+    day_offset <- as.difftime(7*i_print, units = "days")
+    day_current <- start + day_offset
+    
+    url <- "umass.edu"
+    
+    label <- format(day_current, "%b %d")
+    out[i] <- paste("lag: ", i_print, ", date: ", 
+                    day_current, ", label: ", label, 
+                    ", url: ", url, sep = "")
   }
   # upload those somehow and get the URLs
   
