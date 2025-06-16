c5675f02312f8fd926bb559d20ff5df1b869b088
swetha4444 <swethasaseendran4@gmail.com>
2025-06-16 15:16:38 -0400

    feat: add mock API endpoint to entrypoint

--- api/entrypoint.R
+++ api/entrypoint.R
@@ -7,7 +7,8 @@ source("utils/helpers.R")
 # Mount  endpoints
 pr <- pr() %>%
   pr_mount("/hello", plumb("endpoints/hello.R")) %>%
-  pr_mount("/predict", plumb("endpoints/predict.R"))
+  pr_mount("/predict", plumb("endpoints/predict.R")) %>%
+  pr_mount("/mock", plumb("endpoints/mock_api.R"))
 
 # Run the API
 pr$run(host = "0.0.0.0", port = 8000)
