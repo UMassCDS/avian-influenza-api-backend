307fe36844a9ede0afbc17781cd52825a8b49737
swetha4444 <swethasaseendran4@gmail.com>
2025-07-23 18:53:01 -0400

    Remove CORS filter from API entrypoint

--- api/entrypoint.R
+++ api/entrypoint.R
@@ -12,21 +12,8 @@ source("utils/save_json_palette.R")
 source("utils/range_rescale.R")
 source("utils/flow.R")
 
-# Enable CORS for all routes
-cors <- function(req, res) {
-  res$setHeader("Access-Control-Allow-Origin", "*")
-  res$setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-  res$setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
-  if (req$REQUEST_METHOD == "OPTIONS") {
-    res$status <- 200
-    return(list())
-  }
-  forward()
-}
-
 # Mount  endpoints
 pr <- pr() %>%
-  pr_filter(cors) %>%
   pr_mount("/hello", plumb("endpoints/hello.R")) %>%
   pr_mount("/predict", plumb("endpoints/predict.R")) %>%
   pr_mount("/mock", plumb("endpoints/mock_api.R")) %>%
