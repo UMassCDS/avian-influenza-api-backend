b09a77dcb3bd0e4d35539fbba481df27f09c42b9
swetha4444 <swethasaseendran4@gmail.com>
2025-08-05 01:57:04 -0400

    Remove Docker setup instructions from setup_local.sh

--- setup_local.sh
+++ setup_local.sh
@@ -28,19 +28,6 @@ echo "Installing R packages..."
 Rscript -e "install.packages(c('plumber', 'aws.s3', 'remotes', 'Cairo', 'jsonlite'), repos='https://cloud.r-project.org')"
 Rscript -e "remotes::install_github('birdflow-science/BirdFlowR')"
 
-# --- Docker setup ---
-if ! command -v docker &> /dev/null; then
-  echo "Docker not found. Please install Docker Desktop for your OS."
-  exit 1
-fi
-
-echo "Building Docker image..."
-docker build -t avian-flu-api .
-
-echo "Running Docker container (local mode)..."
-mkdir -p ./localtmp
-docker run -p 8000:8000 -v $(pwd)/localtmp:/dev/shm avian-flu-api &
-
 # --- Make git ignore future changes to save_local.flag ---
 git update-index --assume-unchanged api/config/save_local.flag
 echo "Git will now ignore local changes to api/config/save_local.flag."
