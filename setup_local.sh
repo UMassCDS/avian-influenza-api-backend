b19c9b90d836730e940503c5aae60a91ac92eebe
swetha4444 <swethasaseendran4@gmail.com>
2025-08-18 15:15:10 -0400

    Refactor setup_local.sh to streamline BirdFlowAPI installation process

--- setup_local.sh
+++ setup_local.sh
@@ -27,6 +27,13 @@ fi
 echo "Installing R packages..."
 Rscript -e "install.packages(c('plumber', 'aws.s3', 'remotes', 'Cairo', 'jsonlite'), repos='https://cloud.r-project.org')"
 Rscript -e "remotes::install_github('birdflow-science/BirdFlowR')"
+if [ -d "../BirdFlowAPI" ]; then
+  echo "Installing BirdFlowAPI from local source..."
+  Rscript -e "devtools::install('../BirdFlowAPI')"
+else
+  echo "Installing BirdFlowAPI from GitHub..."
+  Rscript -e "remotes::install_github('UMassCDS/BirdFlowAPI')"
+fi
 
 # --- Make git ignore future changes to save_local.flag ---
 git update-index --assume-unchanged api/config/save_local.flag
